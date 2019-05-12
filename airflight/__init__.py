f83a5744eac150a682d49893f1b0a01bcee1082c
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-12 15:51:57 -0300

    Change order of import

--- airflight/__init__.py
+++ airflight/__init__.py
@@ -5,8 +5,8 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
+        config.include('cornice')
         config.include('.collections')
         config.include('.routes')
-        config.include('cornice')
         config.include('.views.api', route_prefix='/api')
     return config.make_wsgi_app()

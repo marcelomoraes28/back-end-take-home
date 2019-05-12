f89f5e2e586a187aad0fee5c34797c03581d6c98
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-12 12:00:18 -0300

    Remove unusual includes

--- airflight/__init__.py
+++ airflight/__init__.py
@@ -5,8 +5,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
-        config.include('.models')
-        config.include('pyramid_jinja2')
+        config.include('.collections')
         config.include('.routes')
-        config.scan()
+        config.scan(ignore='tests')
     return config.make_wsgi_app()

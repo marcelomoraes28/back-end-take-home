13fbdd21fe6baadd9f1ef7716f8ba2a764d7dd85
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-14 10:10:22 -0300

    Implement REST Endpoint

--- airflight/views/api/v1/resources/flight.py
+++ airflight/views/api/v1/resources/flight.py
@@ -1,6 +1,11 @@
 from cornice import Service
 from cornice.validators import marshmallow_querystring_validator
+from pyramid.httpexceptions import HTTPBadRequest, HTTPOk, \
+    HTTPInternalServerError
 
+from airflight.views.api.v1.flight.exceptions import NoFlightFound, \
+    OriginIsTheSameAsTheDestination
+from airflight.views.api.v1.flight.flight import FlightAPI
 from airflight.views.api.v1.marshmallow_schemas.flight_schemas import \
     FlightQuerystringSchema
 
@@ -15,4 +20,12 @@ def _get_flight(request):
     """
     /api/v1/flight?origin=ABJ&destination=BRU
     """
-    # TODO: Implement the endpoint
+    try:
+        get_best_route = FlightAPI(request).get_best_route()
+    except (NoFlightFound, OriginIsTheSameAsTheDestination) as e:
+        return HTTPBadRequest(json=e.args[0])
+    except Exception as e:
+        return HTTPInternalServerError(json={"error": "Internal server error",
+                                             "status": HTTPInternalServerError().status_code})  # noqa
+    else:
+        HTTPOk(json=get_best_route)

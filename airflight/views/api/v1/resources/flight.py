b101782e039503fe81979f46b22e647fb74d889d
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-14 17:01:02 -0300

    Add response_schemas in flight resource

--- airflight/views/api/v1/resources/flight.py
+++ airflight/views/api/v1/resources/flight.py
@@ -1,5 +1,5 @@
 from cornice import Service
-from cornice.validators import marshmallow_querystring_validator
+from cornice.validators import colander_querystring_validator
 from pyramid.httpexceptions import HTTPBadRequest, HTTPOk, \
     HTTPInternalServerError
 
@@ -7,15 +7,18 @@ from airflight.views.api.v1.flight.exceptions import NoFlightFound, \
     OriginIsTheSameAsTheDestination
 from airflight.views.api.v1.flight.flight import FlightAPI
 from airflight.views.api.v1.marshmallow_schemas.flight_schemas import \
-    FlightQuerystringSchema
+    FlightQuerystringSchema, response_route_schemas
 
-flight_service = Service(name='flight_service', path='/flight',
+flight_service = Service(name='flight-service',
+                         path='/flight',
                          description="Get Flights")
 
 
 @flight_service.get(content_type="application/json",
-                    validators=marshmallow_querystring_validator,
-                    schema=FlightQuerystringSchema)
+                    validators=colander_querystring_validator,
+                    schema=FlightQuerystringSchema,
+                    response_schemas=response_route_schemas,
+                    tags=['flight'])
 def _get_flight(request):
     """
     /api/v1/flight?origin=ABJ&destination=BRU
@@ -25,7 +28,8 @@ def _get_flight(request):
     except (NoFlightFound, OriginIsTheSameAsTheDestination) as e:
         return HTTPBadRequest(json=e.args[0])
     except Exception as e:
+        # For security, do not show the traceback to user
         return HTTPInternalServerError(json={"error": "Internal server error",
                                              "status": HTTPInternalServerError().status_code})  # noqa
     else:
-        HTTPOk(json=get_best_route)
+        return HTTPOk(json=get_best_route)

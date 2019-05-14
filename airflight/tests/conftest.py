3eaae19b610b7cede889abaa78a5d58afa50364e
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-14 09:40:58 -0300

    Build a fixture to populate the database

--- airflight/tests/conftest.py
+++ airflight/tests/conftest.py
@@ -19,10 +19,11 @@ def settings():
     get_current_registry().settings = dict(settings)
     return settings
 
+
 @pytest.fixture(scope='session')
 def app(request):
-    with patch('airflight.collections.base.MongoClient', mongomock.MongoClient):
-
+    with patch('airflight.collections.base.MongoClient',
+               mongomock.MongoClient):
         _settings = get_appsettings(settings_file, name='main')
 
         config = testing.setUp(settings=_settings)
@@ -34,3 +35,101 @@ def app(request):
 
         yield test_app
 
+
+@pytest.fixture(scope='session')
+def data(app):
+    from airflight.collections import AirlineCollection, AirportCollection, \
+        RouteCollection
+    _airlines_payload = [
+        {
+            "name": "United Airlines",
+            "2_digit_code": "UA",
+            "3_digit_code": "UAL",
+            "country": "United States"
+        },
+        {
+            "name": "Turkish Airlines",
+            "2_digit_code": "TK",
+            "3_digit_code": "THY",
+            "country": "Turkey"
+        },
+    ]
+    _airports_payload = [
+        {
+            "name": "Etimesgut Air Base",
+            "city": "Ankara",
+            "country": "Turkey",
+            "iata_3": "ANK",
+            "latitute": "39.94979858",
+            "longitude": "32.68859863"
+        },
+        {
+            "name": "Adana Airport",
+            "city": "Adana",
+            "country": "Turkey",
+            "iata_3": "ADA",
+            "latitute": "36.98220062",
+            "longitude": "35.28039932"
+        },
+        {
+            "name": "Victoria Harbour Seaplane Base",
+            "city": "Victoria",
+            "country": "Canada",
+            "iata_3": "YWH",
+            "latitute": "48.42498589",
+            "longitude": "-123.3888674"
+        },
+        {
+            "name": "Lester B. Pearson International Airport",
+            "city": "Toronto",
+            "country": "Canada",
+            "iata_3": "YYZ",
+            "latitute": "43.67720032",
+            "longitude": "-79.63059998"
+        }
+    ]
+    _routes_payload = [
+        {
+            "airline_id": "UA",
+            "origin": "YYZ",
+            "destination": "YWH"
+        },
+        {
+            "airline_id": "UA",
+            "origin": "ANK",
+            "destination": "YYZ"
+        },
+        {
+            "airline_id": "UA",
+            "origin": "YWH",
+            "destination": "ADA"
+        },
+        {
+            "airline_id": "TK",
+            "origin": "YYZ",
+            "destination": "ADA"
+        },
+        {
+            "airline_id": "TK",
+            "origin": "ADA",
+            "destination": "ANK"
+        },
+        {
+            "airline_id": "TK",
+            "origin": "ANK",
+            "destination": "ADA"
+        },
+        {
+            "airline_id": "UA",
+            "origin": "YYZ",
+            "destination": "ANK"
+        },
+        {
+            "airline_id": "UA",
+            "origin": "YWH",
+            "destination": "YYZ"
+        },
+    ]
+    RouteCollection().insert_many(_routes_payload)
+    AirportCollection().insert_many(_airports_payload)
+    AirlineCollection().insert_many(_airlines_payload)

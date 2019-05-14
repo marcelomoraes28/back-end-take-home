725e6ab59bdf09030614b709faa468a675122012
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-14 17:00:18 -0300

    Configure static page and load cornice_swagger module

--- airflight/__init__.py
+++ airflight/__init__.py
@@ -5,8 +5,12 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
+        config.include('pyramid_jinja2')
+        config.add_static_view('static', 'static',
+                               cache_max_age=3600)
         config.include('cornice')
         config.include('.collections')
         config.include('.routes')
         config.include('.views.api', route_prefix='/api')
+        config.include('cornice_swagger')
     return config.make_wsgi_app()

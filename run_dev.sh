3a6cc7434ab51786704a6962a3e66c619476ac71
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-15 07:44:22 -0300

    Fix setup

--- run_dev.sh
+++ run_dev.sh
@@ -8,10 +8,10 @@ else
    python3 -m venv ./venv
    source venv/bin/activate
 fi
-echo 'Installing swagger dependencies'
-npm install --prefix ./airflight/static swagger-ui-dist
 echo 'Install dependencies'
 pip install -e .
+echo 'Installing swagger dependencies'
+npm install --prefix ./airflight/static swagger-ui-dist
 echo 'Migrating the database'
 initialize_airflight_db development.ini
 echo 'Starting the application'
--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
+
 
 requires = [
     'plaster_pastedeploy',
@@ -34,7 +33,7 @@ setup(
     name='airflight',
     version='1.0',
     description='airflight',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=README,
     classifiers=[
         'Programming Language :: Python',
         'Framework :: Pyramid',

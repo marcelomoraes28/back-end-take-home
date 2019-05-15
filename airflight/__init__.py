834eba5a3258efeb8690d2881e90ca3838209fb9
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-15 19:24:40 -0300

    Remove unusual files

--- airflight/__init__.py
+++ airflight/__init__.py
@@ -12,7 +12,6 @@ def main(global_config, **settings):
                                cache_max_age=3600)
         config.include('cornice')
         config.include('.collections')
-        config.include('.routes')
         config.include('.views.api', route_prefix='/api')
         config.include('cornice_swagger')
     return config.make_wsgi_app()
